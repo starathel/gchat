0f0ec4547ecdb7f69c296a9414cc5ebc40d14e0b
starathel <starathel.dev@gmail.com>
2025-03-04 17:04:12 +0300

    Working something

--- Makefile
+++ Makefile
@@ -1,8 +1,24 @@
-.PHONY: server tidy
-server:
+.PHONY: server tidy proto clean all build_server build
+all: build
+
+server: build_server
+	./bin/server
+
+build: build_server
+
+build_server:
 	mkdir -p bin
 	go build -o bin/server cmd/server/server.go
+
+proto:
+	mkdir -p gen
+	protoc --go_out=gen --go_opt=module=github.com/starathel/gchat/gen \
+		--go-grpc_out=gen --go-grpc_opt=module=github.com/starathel/gchat/gen \
+		proto/*.proto
 
 tidy:
 	go mod tidy
 	go mod vendor
+
+clean:
+	rm -rf bin
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,10 +1,94 @@
 package main
 
 import (
-    "fmt"
-    "google.golang.org/grpc"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"slices"
+	"sync"
+
+	"github.com/starathel/gchat/gen/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type MessageStream = grpc.BidiStreamingServer[hello.HelloMessage, hello.HelloMessage]
+
+type HelloServer struct {
+	hello.UnimplementedHelloServiceServer
+	mu                sync.RWMutex
+	active_streams    []MessageStream
+	incoming_messages chan *hello.HelloMessage
+}
+
+func (s *HelloServer) SayHello(_ context.Context, msg *hello.HelloMessage) (*hello.HelloMessage, error) {
+	if len(msg.GetUsername()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Username should be not empty")
+	}
+	return &hello.HelloMessage{
+		Username: "server",
+		Text:     fmt.Sprintf("Hello %s. From server", msg.Username),
+	}, nil
+}
+
+func (s *HelloServer) Chat(stream MessageStream) error {
+	s.mu.Lock()
+	s.active_streams = append(s.active_streams, stream)
+	s.mu.Unlock()
+
+	end := make(chan struct{})
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				s.mu.Lock()
+				s.active_streams = slices.DeleteFunc(s.active_streams, func(s MessageStream) bool { return s == stream })
+				s.mu.Unlock()
+				end <- struct{}{}
+				return
+			}
+			s.incoming_messages <- msg
+		}
+	}()
+	<-end
+	return nil
+}
+
+func (s *HelloServer) processMessages() {
+	for msg := range s.incoming_messages {
+		s.mu.RLock()
+		for _, stream := range s.active_streams {
+			err := stream.Send(msg)
+			if err != nil {
+				fmt.Println("WARNING: Attempt to write into closed stream")
+				continue
+			}
+		}
+		s.mu.RUnlock()
+	}
+}
+
+func NewHelloServer() *HelloServer {
+	server := HelloServer{}
+	server.incoming_messages = make(chan *hello.HelloMessage, 10)
+	return &server
+}
+
 func main() {
-    fmt.Println("hello world,", grpc.Version)
+	l, err := net.Listen("tcp", "127.0.0.1:6969")
+	if err != nil {
+		log.Fatalf("cannot start listening on 6969: %v", err)
+	}
+	fmt.Println("Started listener")
+	grpcServer := grpc.NewServer()
+	helloServer := NewHelloServer()
+	hello.RegisterHelloServiceServer(grpcServer, helloServer)
+	go helloServer.processMessages()
+	err = grpcServer.Serve(l)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Started server")
 }
